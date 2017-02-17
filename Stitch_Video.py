3a0668f7bbb5e4b20a17cd4231a9083c04dd1e20
rslinford <scott@linfords.com>
2017-02-17 10:03:51 -0800

    Writes to AVI video format at real time

--- Stitch_Video.py
+++ Stitch_Video.py
@@ -1,24 +1,40 @@
 import glob
 import os
 import cv2
+from datetime import datetime
 import rsl.json_config as json_config
 
 default_config = {
     'config_file_name': r'stitch_video_config.json',
-    'source_dir_name': r'C:\sourcedir'
+    'fourcc_text': 'XVID',
+    'source_dir_name': r'C:\source_dir'
 }
 
 
+def get_cap_prop_size(cap):
+    return int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+
 def stitch_video(config):
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    target_filename = '%s_stitched.avi' % timestamp
+    target_fullpath = os.path.join(config['source_dir_name'], target_filename)
+    fourcc = cv2.VideoWriter_fourcc(*config['fourcc_text'])
+    video = None
     for source_filename in glob.iglob(r'%s\*.mp4' % config['source_dir_name']):
         print('Reading: %s' % source_filename)
         source_fullpath = os.path.join(config['source_dir_name'], source_filename)
         cap = cv2.VideoCapture(source_fullpath)
+        if video is None:
+            original_fps = cap.get(cv2.CAP_PROP_FPS)
+            frame_width, frame_height = get_cap_prop_size(cap)
+            video = cv2.VideoWriter(target_fullpath, fourcc, original_fps, (frame_width, frame_height), True)
         while True:
             (grabbed, current_frame) = cap.read()
             # if the frame could not be grabbed, then we have reached the end of the video
             if not grabbed:
                 break
+            video.write(current_frame)
             cv2.imshow('Source Video', current_frame)
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
